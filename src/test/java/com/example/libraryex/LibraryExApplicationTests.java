9ca11bca51709c0c84d52353040b5035e557ffd9
Seoha-Yoon <seohayn127@gmail.com>
2022-02-28 22:10:44 +0900

    first commit

---
+++ src/test/java/com/example/libraryex/LibraryExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
