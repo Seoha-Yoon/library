354b5c1a4841ae2d682c87c1cf0126633d136ee7
Seoha-Yoon <seohayn127@gmail.com>
2022-04-21 15:01:07 +0900

    이름 검색 vs 인덱스 검색 테스트

--- library/src/main/java/ysh/library/repository/BookRepository.java
+++ library/src/main/java/ysh/library/repository/BookRepository.java
@@ -6,6 +6,8 @@ import ysh.library.domain.Book;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -14,6 +16,13 @@ public class BookRepository {
     private final EntityManager em;
 
     public Book findOne(Long id){ return em.find(Book.class, id);}
+
+    public Optional<Book> findByBookName(String title){
+        List<Book> book = em.createQuery("select b from Book b where b.title= :title", Book.class)
+                .setParameter("title", title)
+                .getResultList();
+        return book.stream().findAny();
+    }
 
     public List<Book> findAll(){
         return em.createQuery("select b from Book b", Book.class)
---
+++ library/src/test/java/ysh/library/book/FindBookTest.java
@@ -0,0 +1,39 @@
+package ysh.library.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
+import ysh.library.repository.BookRepository;
+
+@SpringBootTest
+@Transactional
+public class FindBookTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void test() throws Exception{
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        bookRepository.findOne((long)9);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+
+        StopWatch stopWatch2 = new StopWatch();
+        stopWatch2.start();
+        bookRepository.findByBookName("고구려");
+        stopWatch2.stop();
+        System.out.println(stopWatch2.prettyPrint());
+
+        long time1 = stopWatch.getTotalTimeMillis();
+        long time2 = stopWatch2.getTotalTimeMillis();
+
+        if(time1 < time2){
+            System.out.println("인덱스 활용해서 검색하는 것이 "+time2/time1+"배 더 빠름");
+       }
+
+    }
+}
